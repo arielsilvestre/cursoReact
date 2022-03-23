84e2c8e26781a6349850c2324828ec92073d51eb
arielsilvestre <silvestreariel@gmail.com>
2022-03-23 20:59:13 -0300

    entrega 2

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,13 @@
+
+function NavBar() {
+    return(
+    <>
+            <h2>Nombre De empresa</h2>
+            <button>boton_1</button>
+            <button>boton_2</button>
+            <button>boton_3</button>
+    </>
+    )
+}
+
+export default NavBar
