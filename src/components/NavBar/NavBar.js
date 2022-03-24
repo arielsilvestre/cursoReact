0819c4ce79becf1d871f8dfa00ac90d06a8ab4c8
arielsilvestre <silvestreariel@gmail.com>
2022-03-24 11:54:56 -0300

    Cambios en la barra de tarea

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,12 +1,12 @@
-
+import './NavBar.css';
 function NavBar() {
     return(
-    <>
+    <div className='navBar'>
             <h2>Nombre De empresa</h2>
             <button>boton_1</button>
             <button>boton_2</button>
             <button>boton_3</button>
-    </>
+    </div>
     )
 }
 
